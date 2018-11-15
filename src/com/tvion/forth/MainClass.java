c770a0b42182326ecc640a2ddee5118624cc389f
Tvion <snakersu@gmail.com>
2018-11-15 14:55:28 +0300

    Code review

--- src/com/tvion/forth/MainClass.java
+++ src/com/tvion/forth/MainClass.java
@@ -1,7 +1,6 @@
 package com.tvion.forth;
 
 import com.tvion.second.MyComplex;
-
 import java.util.LinkedList;
 import java.util.List;
 
@@ -19,9 +18,10 @@ public class MainClass {
             System.out.println(c);
         }
         MyComplex l=new MyComplex(6,44);
-        System.out.println(myLinkedList.indexOf(l));
+        System.out.println("Index 4+44i element is "+myLinkedList.indexOf(l));
+        System.out.println("Add 0+0 on index 3");
         myLinkedList.add(3,new MyComplex(0,0));
-        System.out.println(myLinkedList.get(1));
+        System.out.println("Get 2 element "+myLinkedList.get(1));
         System.out.println();
         for(MyComplex c:myLinkedList){
             System.out.println(c);
@@ -29,12 +29,12 @@ public class MainClass {
 
 
         MyComplex mc=new MyComplex(3,47);
-        System.out.println(myLinkedList.indexOf(mc));
+        System.out.println("Index of 3+47i is "+myLinkedList.indexOf(mc));
         System.out.println(myLinkedList);
         System.out.println();
 
 
-        System.out.println("*****COMPARE_WITH_STANDART*****");
+        System.out.println("*****COMPARE_WITH_STANDARD*****");
         List<MyComplex> libraryLinkedList= new LinkedList<>();
         myLinkedList.clear();
         for(int i=0;i<10000;i++){
@@ -58,13 +58,11 @@ public class MainClass {
         startTime=System.nanoTime();
         myLinkedList.remove(ind);
         estimatedTime=System.nanoTime()-startTime;
-        System.out.println(myLinkedList.get(ind));
         System.out.println("MyList remove target at "+estimatedTime+" time");
         startTime=System.nanoTime();
         libraryLinkedList.remove(ind);
         estimatedTime=System.nanoTime()-startTime;
         System.out.println("LibraryList remove target at "+estimatedTime+" time");
-        System.out.println(libraryLinkedList.get(ind));
         System.out.println();
 
 
@@ -78,14 +76,6 @@ public class MainClass {
         estimatedTime=System.nanoTime()-startTime;
         System.out.println("LibraryList add target at "+estimatedTime+" time");
         System.out.println();
-        System.out.println(libraryLinkedList.get(ind));
-        for(int i=0;i<myLinkedList.size();i++){
-            System.out.println(myLinkedList.get(i));
-
-        }
-
-
-
     }
 
 
