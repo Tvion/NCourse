4e4e93e89f1075a1c0c57abd69ac6849ee193206
Tvion <snakersu@gmail.com>
2018-11-16 15:46:21 +0300

    Code refactor

--- src/com/tvion/forth/MainClass.java
+++ src/com/tvion/forth/MainClass.java
@@ -1,16 +1,24 @@
 package com.tvion.forth;
 
+import com.tvion.first.MyTriangle;
 import com.tvion.second.MyComplex;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
 
 public class MainClass {
-
+    public static long startTime;
+    public static long estimatedTime;
+    public static List<MyComplex> libraryLinkedList;
+    public static ILinkedList<MyComplex> myLinkedList;
     public static void main(String[] args) {
         long startTime;
         long estimatedTime;
-        ILinkedList<MyComplex> myLinkedList=new MyLinkedList<>();
+        myLinkedList=new MyLinkedList<>();
         for(int i=0;i<10;i++){
             myLinkedList.add(new MyComplex(i,50-i));
         }
@@ -32,17 +40,18 @@ public class MainClass {
         System.out.println("Index of 3+47i is "+myLinkedList.indexOf(mc));
         System.out.println(myLinkedList);
         System.out.println();
+compareWithStandard();
+    }
 
-
+    public static void compareWithStandard(){
         System.out.println("*****COMPARE_WITH_STANDARD*****");
-        List<MyComplex> libraryLinkedList= new LinkedList<>();
+        libraryLinkedList= new LinkedList<>();
         myLinkedList.clear();
         for(int i=0;i<10000;i++){
             myLinkedList.add(new MyComplex(i,50-i));
-        }
-        for(int i=0;i<10000;i++){
             libraryLinkedList.add(new MyComplex(i,50-i));
         }
+
         MyComplex target;
         int ind=6000;
         startTime=System.nanoTime();
@@ -78,5 +87,17 @@ public class MainClass {
         System.out.println();
     }
 
-
+    public static void doForList(Collection col, String operation, MyTriangle... mt) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class colClass = col.getClass();
+        Method method = colClass.getMethod(operation, Object.class);
+        method.setAccessible(true);
+        String[] name = colClass.getName().split("\\.");
+        startTime = System.nanoTime();
+        for (int i = 0; i < mt.length; i++) {
+            method.invoke(col, mt[i]);
+        }
+        estimatedTime = System.nanoTime() - startTime;
+        System.out.println(name[name.length - 1] + " " + operation + " " + mt.length + " elements time is");
+        System.out.println(estimatedTime);
+    }
 }
