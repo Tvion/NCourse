575b0f8d8489cb9327d734baaf819d6e0fd83b6f
Tvion <snakersu@gmail.com>
2018-11-16 16:14:28 +0300

    Code refactor

--- src/com/tvion/forth/MainClass.java
+++ src/com/tvion/forth/MainClass.java
@@ -19,6 +19,7 @@ public class MainClass {
     public static final String[] operations = {"get", "add to index", "remove"};
 
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        //Создаем наш список
         myLinkedList = new MyLinkedList<>();
         for (int i = 0; i < 10; i++) {
             myLinkedList.add(new MyComplex(i, 50 - i));
@@ -26,21 +27,25 @@ public class MainClass {
         for (MyComplex c : myLinkedList) {
             System.out.println(c);
         }
+
+        //Изменяем
         MyComplex l = new MyComplex(6, 44);
         System.out.println("Index 4+44i element is " + myLinkedList.indexOf(l));
         System.out.println("Add 0+0 on index 3");
         myLinkedList.add(3, new MyComplex(0, 0));
         System.out.println("Get 2 element " + myLinkedList.get(1));
         System.out.println();
+
+        //Выводим для сравнения
         for (MyComplex c : myLinkedList) {
             System.out.println(c);
         }
-
 
         MyComplex mc = new MyComplex(3, 47);
         System.out.println("Index of 3+47i is " + myLinkedList.indexOf(mc));
         System.out.println(myLinkedList);
         System.out.println();
+
         compareWithStandard();
     }
 
