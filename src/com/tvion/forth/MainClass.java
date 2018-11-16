8ef5fb6bbaae97c2cc2645ca6a248ebcc6096c69
Tvion <snakersu@gmail.com>
2018-11-16 16:12:06 +0300

    Code refactor

--- src/com/tvion/forth/MainClass.java
+++ src/com/tvion/forth/MainClass.java
@@ -1,11 +1,9 @@
 package com.tvion.forth;
 
-import com.tvion.first.MyTriangle;
 import com.tvion.second.MyComplex;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,89 +13,88 @@ public class MainClass {
     public static long estimatedTime;
     public static List<MyComplex> libraryLinkedList;
     public static ILinkedList<MyComplex> myLinkedList;
-    public static void main(String[] args) {
-        long startTime;
-        long estimatedTime;
-        myLinkedList=new MyLinkedList<>();
-        for(int i=0;i<10;i++){
-            myLinkedList.add(new MyComplex(i,50-i));
+    public static final int ind = 6000;
+    public static final int size = 10000;
+    public static final int count = 10000;
+    public static final String[] operations = {"get", "add to index", "remove"};
+
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        myLinkedList = new MyLinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            myLinkedList.add(new MyComplex(i, 50 - i));
         }
-        for(MyComplex c:myLinkedList){
+        for (MyComplex c : myLinkedList) {
             System.out.println(c);
         }
-        MyComplex l=new MyComplex(6,44);
-        System.out.println("Index 4+44i element is "+myLinkedList.indexOf(l));
+        MyComplex l = new MyComplex(6, 44);
+        System.out.println("Index 4+44i element is " + myLinkedList.indexOf(l));
         System.out.println("Add 0+0 on index 3");
-        myLinkedList.add(3,new MyComplex(0,0));
-        System.out.println("Get 2 element "+myLinkedList.get(1));
+        myLinkedList.add(3, new MyComplex(0, 0));
+        System.out.println("Get 2 element " + myLinkedList.get(1));
         System.out.println();
-        for(MyComplex c:myLinkedList){
+        for (MyComplex c : myLinkedList) {
             System.out.println(c);
         }
 
 
-        MyComplex mc=new MyComplex(3,47);
-        System.out.println("Index of 3+47i is "+myLinkedList.indexOf(mc));
+        MyComplex mc = new MyComplex(3, 47);
+        System.out.println("Index of 3+47i is " + myLinkedList.indexOf(mc));
         System.out.println(myLinkedList);
         System.out.println();
-compareWithStandard();
+        compareWithStandard();
     }
 
-    public static void compareWithStandard(){
-        System.out.println("*****COMPARE_WITH_STANDARD*****");
-        libraryLinkedList= new LinkedList<>();
-        myLinkedList.clear();
-        for(int i=0;i<10000;i++){
-            myLinkedList.add(new MyComplex(i,50-i));
-            libraryLinkedList.add(new MyComplex(i,50-i));
+    public static void compareWithStandard() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        MyComplex[] myComplex = new MyComplex[count];
+        for (int i = 0; i < count; i++) {
+            myComplex[i] = new MyComplex(i * i, i - 100);
         }
 
-        MyComplex target;
-        int ind=6000;
-        startTime=System.nanoTime();
-        target=myLinkedList.get(ind);
-        estimatedTime=System.nanoTime()-startTime;
-        System.out.println("MyList find target= "+target+" at "+estimatedTime+" time");
-        startTime=System.nanoTime();
-        target=libraryLinkedList.get(ind);
-        estimatedTime=System.nanoTime()-startTime;
-        System.out.println("LibraryList find target= "+target+" at "+estimatedTime+" time");
-        System.out.println();
+        System.out.println("*****COMPARE_WITH_STANDARD*****");
 
-        startTime=System.nanoTime();
-        myLinkedList.remove(ind);
-        estimatedTime=System.nanoTime()-startTime;
-        System.out.println("MyList remove target at "+estimatedTime+" time");
-        startTime=System.nanoTime();
-        libraryLinkedList.remove(ind);
-        estimatedTime=System.nanoTime()-startTime;
-        System.out.println("LibraryList remove target at "+estimatedTime+" time");
-        System.out.println();
+        libraryLinkedList = new LinkedList<>();
+        myLinkedList.clear();
+
+        for (int i = 0; i < size; i++) {
+            myLinkedList.add(new MyComplex(i, 50 - i));
+            libraryLinkedList.add(new MyComplex(i, 50 - i));
+        }
 
 
-        MyComplex p=new MyComplex(10,10);
-        startTime=System.nanoTime();
-        myLinkedList.add(ind,p);
-        estimatedTime=System.nanoTime()-startTime;
-        System.out.println("MyList add target at "+estimatedTime+" time");
-        startTime=System.nanoTime();
-        libraryLinkedList.add(ind,p);
-        estimatedTime=System.nanoTime()-startTime;
-        System.out.println("LibraryList add target at "+estimatedTime+" time");
-        System.out.println();
+        for (String operation : operations) {
+            doForList(myLinkedList, operation, myComplex);
+            doForList(libraryLinkedList, operation, myComplex);
+        }
+
+
     }
 
-    public static void doForList(Collection col, String operation, MyTriangle... mt) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public static void doForList(Object col, String operation, MyComplex... myComplexes) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Class colClass = col.getClass();
-        Method method = colClass.getMethod(operation, Object.class);
+        Method method;
+        if ("add to index".equals(operation)) {
+            method = colClass.getMethod("add", int.class, Object.class);
+        } else {
+            method = colClass.getMethod(operation, int.class);
+        }
         method.setAccessible(true);
         String[] name = colClass.getName().split("\\.");
-        startTime = System.nanoTime();
-        for (int i = 0; i < mt.length; i++) {
-            method.invoke(col, mt[i]);
+        if ("add to index".equals(operation)) {
+            startTime = System.nanoTime();
+            for (int i = 0; i < myComplexes.length; i++) {
+                method.invoke(col, ind, myComplexes[i]);
+            }
+            estimatedTime = System.nanoTime() - startTime;
+        } else {
+            startTime = System.nanoTime();
+            for (int i = 0; i < myComplexes.length; i++) {
+                method.invoke(col, ind);
+            }
+            estimatedTime = System.nanoTime() - startTime;
         }
-        estimatedTime = System.nanoTime() - startTime;
-        System.out.println(name[name.length - 1] + " " + operation + " " + mt.length + " elements time is");
+        System.out.println(name[name.length - 1] + " " + operation + " " + myComplexes.length + " elements time is");
         System.out.println(estimatedTime);
+        System.out.println();
     }
+
 }
